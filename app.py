89a1dc0aa299214388e1f688661f3a3745211a0c
Akshay Nachappa <akshaynachappa@gmail.com>
2021-06-05 15:55:48 +0530

    imported files

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+    
+
+@app.route("/single")
+def single():
+    return render_template("single.html")
+
+
+
+
+
